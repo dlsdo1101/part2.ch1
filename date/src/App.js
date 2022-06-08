8367da5431e2a0eabcd5ccdff7b1f2482073c880
김인애 <dlsdo1101@naver.com>
2022-06-08 10:30:27 +0900

    first commit

---
+++ date/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import MomentExample from "./components/Moment/MomentExample";
+
+function App() {
+  return (
+    <div className="App">
+      <MomentExample />
+    </div>
+  );
+}
+
+export default App;
